de5dce5409d81ce8fb75c2a6160d95d9e9344ed8
Paul Anthony McGowan <xhostcom@gmail.com>
2021-11-01 19:47:41 +0000

    SeedDB more

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
-d = dish.measured_ingredients.create(
+#Ingredients = sample_model(Ingredient, IngredientsHelper)
+
+Ingredients.each do |ingredient|
+  dish.measured_ingredients.create(
     amount: rand(MEASUREMENT_AMOUNT_RANGE),
     ingredient: ingredient,
     measurement: measurements.sample
