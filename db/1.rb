325d6efba19b9be419e0f917ed25586ac09db512
Paul Anthony McGowan <xhostcom@gmail.com>
2021-11-01 18:48:39 +0000

    SeedDB

---
+++ db/1.rb
@@ -0,0 +1,55 @@
+MEASUREMENTS = %i[millilitre litre milligram gram].freeze
+MEASUREMENTS.each do |measurement|
+  Measurement.create(name: measurement)
+end
+
+NUMBER_OF_INGREDIENTS = 100
+NUMBER_OF_RESTAURANTS = 10
+DISHES_PER_RESTAURANT = 5
+INGREDIENTS_PER_DISH = 6
+MEASUREMENT_AMOUNT_RANGE = (1...100).freeze
+def creation_check(model, expected_count)
+  if expected_count != model.count
+    puts "#{model} Issue! Wanted #{expected_count} Created #{model.count}"
+  else
+    puts "#{model.count} #{model} Objects Created"
+  end
+end
+def sample_model(model, count)
+  model.offset(rand(model.count - count)).limit(count)
+end
+MEASUREMENTS.each do |measurement|
+  Measurement.create(name: measurement)
+end
+
+measurements = Measurement.all
+
+NUMBER_OF_INGREDIENTS.times do
+  Ingredient.create(name: Faker::Food.unique.ingredient)
+end
+NUMBER_OF_RESTAURANTS.times do
+  restaurant = Restaurant.create(name: Faker::Restaurant.name, description: Faker::Lorem.characters(number: 10))
+
+  DISHES_PER_RESTAURANT.times do
+    dish = restaurant.dishes.create(name: Faker::Food.unique.dish)
+    #downloaded_image = URI.call("https://source.unsplash.com/600x600/?#{dish.name}")
+    # dish.image.attach(io: downloaded_image, filename: "image-#{dish.name}.jpg")
+
+    Ingredients = sample_model(Ingredient, INGREDIENTS_PER_DISH)
+
+    Ingredients.each do |ingredient|
+      dish.measured_ingredients.create(
+        amount: rand(MEASUREMENT_AMOUNT_RANGE),
+        ingredient: ingredient,
+        measurement: measurements.sample
+      )
+    end
+  end
+end
+creation_check(Measurement, MEASUREMENTS.size)
+creation_check(Restaurant, NUMBER_OF_RESTAURANTS)
+creation_check(Ingredient, NUMBER_OF_INGREDIENTS)
+creation_check(Dish, NUMBER_OF_RESTAURANTS * DISHES_PER_RESTAURANT)
+creation_check(MeasuredIngredient, NUMBER_OF_RESTAURANTS * DISHES_PER_RESTAURANT * INGREDIENTS_PER_DISH)
+
+
